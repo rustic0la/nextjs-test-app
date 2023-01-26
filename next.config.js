4f2bbc3af28bbff819b1a91dc695a1c05ac9f1f8
rustic0la <vm853404@protonmail.com>
2023-01-26 11:45:35 +0300

    setup for the first run

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      loader: "@svgr/webpack",
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  // disable plugins
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/i,
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
