35e764b648624f83bdf6240742accbe778020e2e
rustic0la <vm853404@protonmail.com>
2023-01-30 01:05:57 +0300

    formatting

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,18 +1,19 @@
-import { FC, HTMLAttributes } from "react";
-import styles from "./Tag.module.css";
-import cn from "classnames";
-import { DetailedHTMLProps, ReactNode } from "react";
+import { FC, HTMLAttributes } from 'react';
+import styles from './Tag.module.css';
+import cn from 'classnames';
+import { DetailedHTMLProps, ReactNode } from 'react';
 
 export interface TagProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   children: ReactNode;
-  size?: "s" | "m";
-  color: "ghost" | "red" | "grey" | "green" | "primary";
+  size?: 's' | 'm';
+  color: 'ghost' | 'red' | 'grey' | 'green' | 'primary';
+  href?: string;
 }
 
 export const Tag: FC<TagProps> = ({
   children,
-  color = "ghost",
+  color = 'ghost',
   className,
   size,
   href,
@@ -21,13 +22,13 @@ export const Tag: FC<TagProps> = ({
   return (
     <p
       className={cn(styles.tag, className, {
-        [styles.s]: size === "s",
-        [styles.m]: size === "m",
-        [styles.ghost]: color === "ghost",
-        [styles.red]: color === "red",
-        [styles.grey]: color === "grey",
-        [styles.green]: color === "green",
-        [styles.primary]: color === "primary",
+        [styles.s]: size === 's',
+        [styles.m]: size === 'm',
+        [styles.ghost]: color === 'ghost',
+        [styles.red]: color === 'red',
+        [styles.grey]: color === 'grey',
+        [styles.green]: color === 'green',
+        [styles.primary]: color === 'primary',
       })}
       {...props}
     >
